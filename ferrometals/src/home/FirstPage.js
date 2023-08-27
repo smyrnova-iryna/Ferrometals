d32f5dda8b60bd2bf5fa6f1953f00720b4da7a0f
Ira1234567 <iraowl1234@gmail.com>
2023-08-27 20:07:17 +0300

    Update

--- ferrometals/src/home/FirstPage.js
+++ ferrometals/src/home/FirstPage.js
@@ -1,17 +1,17 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 
 const FirstPage = ({data, handleToggle, navigateToTheTop, currentViewportHeight, currentViewportWidthIsLarger, 
 currentViewportWidth}) => {
     return (
         <article className='First-Page-Main-Container' style={{minHeight: currentViewportHeight, padding: "0"}}>
-            <div className='First-Page-Picture-Container' style={{height: `${currentViewportHeight*0.8}px`, marginLeft: 
+            {/* <div className='First-Page-Picture-Container' style={{height: `${currentViewportHeight*0.8}px`, marginLeft: 
             `${377.27272727273 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
             Number(currentViewportWidthIsLarger) / 2}px`, width: `${currentViewportWidth - (377.27272727273 / 746 * 
             currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * Number(currentViewportWidthIsLarger) / 2)}px`}}>
-            </div>
+            </div> */}
             {/* height: `${currentViewportHeight*0.8}px` */}
-            <div className='First-Page-Button-Container' style={{height: `${currentViewportHeight*0.2}px`, 
+            {/* <div className='First-Page-Button-Container' style={{height: `${currentViewportHeight*0.2}px`, 
             top: `${currentViewportHeight*0.8}px`}}>
                     <button onClick={handleToggle} style={{zIndex: "95"}} className='FirstPage-Title-Button'>
                     {data.home.firstPage.button}
@@ -19,7 +19,7 @@ currentViewportWidth}) => {
                     <Link to={data.menuPaths.catalog} style={{zIndex: "95"}} className='FirstPage-Title-Link' 
                     onClick={navigateToTheTop}>{data.home.firstPage.link}
                     </Link>
-            </div>   
+            </div>    */}
             <svg className='First-Page-Svg' xmlns="http://www.w3.org/2000/svg" style={{fill:"url(#FirstDecorElementGradient)", 
             position: "absolute", width: "100%", top: "0", right: "0", height: "100%"}}>
                 <linearGradient id="FirstDecorElementGradient">
@@ -37,7 +37,7 @@ currentViewportWidth}) => {
                 Sorry, your browser does not support inline SVG.
             </svg> 
             {/* marginTop: `${550.76310550763 / 746 * currentViewportHeight / 20}px`, */}
-            <div className='First-Page-Logo-Extra-Container' style={{height: `${550.76310550763 / 746 * currentViewportHeight}px`, 
+            {/* <div className='First-Page-Logo-Extra-Container' style={{height: `${550.76310550763 / 746 * currentViewportHeight}px`, 
             width: `${512.2096881221 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
             Number(currentViewportWidthIsLarger) / 2}px`}}>
                 <div className='First-Page-Logo-Container' style={{maxHeight: `${550.76310550763 / 746 * currentViewportHeight}px`, 
@@ -53,7 +53,7 @@ currentViewportWidth}) => {
                     / 5}px`, width: `${473.65627073656 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight 
                     * Number(currentViewportWidthIsLarger) / 2 - 40}px`}}>{data.home.firstPage.description}</p>
                 </div>
-            </div>
+            </div> */}
             <svg className='First-Page-Svg' style={{fill:"url(#SecondDecorElementGradient)", position: "absolute", width: "100%", 
             top: "0", right: "0", height: "100%"}}>
                 <linearGradient id="SecondDecorElementGradient">
@@ -79,14 +79,6 @@ currentViewportWidth}) => {
                     <stop offset="30%" stopColor="rgba(48,48,49,255)" />
                     <stop offset="90%" stopColor="rgba(138,137,136,255)" />
                 </linearGradient>
-                {/* <polygon points={`
-                0,${523.22495023225 / 746 * currentViewportHeight} 
-                ${495.68679495687 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
-                Number(currentViewportWidthIsLarger) / 2},${523.22495023225 / 746 * currentViewportHeight} 
-                ${616.85467816855 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
-                Number(currentViewportWidthIsLarger) / 2},${749.03782349038 / 746 * currentViewportHeight} 
-                0,${749.0378234903 / 746 * currentViewportHeight}
-                `} /> */}
                 <polygon points={`
                 0,${523.22495023225 / 746 * currentViewportHeight} 
                 ${495.68679495687 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
@@ -98,7 +90,7 @@ currentViewportWidth}) => {
                 Sorry, your browser does not support inline SVG.
             </svg> 
             {/* "0,523.22495023225 495.68679495687,523.22495023225 616.85467816855,749.03782349038 0,749.0378234903" */}
-            <div className='First-Page-List-Container' style={{ top: `${523.22495023225 / 746 * currentViewportHeight}px`, 
+            {/* <div className='First-Page-List-Container' style={{ top: `${523.22495023225 / 746 * currentViewportHeight}px`, 
             height: `${(749.0378234903 - 523.22495023225) / 746 * currentViewportHeight}px`, width: `${495.68679495687 / 746 * 
             currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * Number(currentViewportWidthIsLarger) / 2}px`}}>
                 <ul className='First-Page-List'>
@@ -109,14 +101,6 @@ currentViewportWidth}) => {
                     )}
                 </ul> 
             </div>
-            {/* <div className='First-Page-Button-Block'>
-                    <button onClick={handleToggle} style={{zIndex: "102"}} className='FirstPage-Title-Button'>
-                    {data.home.firstPage.button}
-                    </button>
-                    <Link to={data.menuPaths.catalog} style={{zIndex: "102"}} className='FirstPage-Title-Link' 
-                    onClick={navigateToTheTop}>{data.home.firstPage.link}
-                    </Link>
-            </div>  */}
             <div className='First-Page-Button-Container-Adaptive'>
                     <button onClick={handleToggle} style={{zIndex: "102", backgroundColor: "pink"}} className='FirstPage-Title-Button'>
                     {data.home.firstPage.button}
@@ -124,7 +108,7 @@ currentViewportWidth}) => {
                     <Link to={data.menuPaths.catalog} style={{zIndex: "102"}} className='FirstPage-Title-Link' 
                     onClick={navigateToTheTop}>{data.home.firstPage.link}
                     </Link>
-            </div> 
+            </div>  */}
         </article>
     )
 }
