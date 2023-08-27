07317391837b18d4e8d73a0c478930a81649b0d2
Ira1234567 <iraowl1234@gmail.com>
2023-08-27 16:18:23 +0300

    Update

--- ferrometals/src/home/FirstPage.js
+++ ferrometals/src/home/FirstPage.js
@@ -90,7 +90,7 @@ currentViewportWidth}) => {
                 Sorry, your browser does not support inline SVG.
             </svg> 
             {/* "0,523.22495023225 495.68679495687,523.22495023225 616.85467816855,749.03782349038 0,749.0378234903" */}
-            <div className='First-Page-List-Container' style={{backgroundColor: "red", top: `${523.22495023225 / 746 * currentViewportHeight}px`, 
+            <div className='First-Page-List-Container' style={{ top: `${523.22495023225 / 746 * currentViewportHeight}px`, 
             height: `${(749.0378234903 - 523.22495023225) / 746 * currentViewportHeight}px`, width: `${495.68679495687 / 746 * 
             currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * Number(currentViewportWidthIsLarger) / 2}px`}}>
                 <ul className='First-Page-List'>
