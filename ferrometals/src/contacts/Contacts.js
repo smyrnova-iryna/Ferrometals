1df437bd3a82abffc4e4ddcd4ed8cd51e81256fa
Ira1234567 <iraowl1234@gmail.com>
2023-09-02 21:05:52 +0300

    Update

--- ferrometals/src/contacts/Contacts.js
+++ ferrometals/src/contacts/Contacts.js
@@ -110,6 +110,34 @@ const Contacts = ({data, currentViewportHeight, currentViewportWidthIsLarger, cu
             </div>
 
 
+            <div className='First-Page-Logo-Extra-Container' >
+                <div className='Contacts-Inner-Container' style={{height: `${550.76310550763 / 746 * currentViewportHeight}px`, 
+                maxWidth: `${512.2096881221 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
+                Number(currentViewportWidthIsLarger) / 2}px`}}>
+                    <div className="Contacts-inner-content-container">
+                                <h4 className="Contacts-inner-container-heading">{data.contacts.address.heading}</h4>
+                            <div className="Contacts-inner-content-container Contacts-Items-Container">
+                                <div className="Contacts-text-container" style={{maxWidth: `${512.2096881221 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
+                Number(currentViewportWidthIsLarger) / 2}px`}}><p className="Contacts-item">
+                                        <img className="Contacts-icon" alt="Icon" src={require(`../img/contacts_location_icon.png`)}></img>
+                                        {data.contacts.address.text}</p></div>
+                                <div><p className="Contacts-item"><a className="Contacts-item" href={fullPhoneNumber}>
+                                        <img className="Contacts-icon" alt="Icon" src={require(`../img/contacts_call_icon.png`)}></img>
+                                        {data.header.phoneNumber}</a></p></div>
+                                <div><p className="Contacts-item"><a className="Contacts-item" href={fullEmail}>
+                                        <img className="Contacts-icon" alt="Icon" src={require(`../img/contacts_email_icon.png`)}></img>
+                                        {data.header.email}</a></p></div>
+                            </div>
+                    </div>
+                    <div className="Contacts-inner-content-container Contacts-Bottom-Text-Container">
+                                <h4 className="Contacts-inner-container-heading Contacts-second-inner-container-heading">{data.contacts.address.detailsHeading}</h4>
+                                <p style={{maxWidth: `${512.2096881221 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
+                Number(currentViewportWidthIsLarger) / 2}px`}} className="Contacts-inner-container-text">{data.contacts.address.details}</p>
+                    </div>
+                </div>
+            </div>
+
+
                 {/* <div className='Contacts-feedback-form-adaptive-button-container' style={{top: `${523.22495023225 / 746 * currentViewportHeight}px`, 
             height: `${(749.0378234903 - 523.22495023225) / 746 * currentViewportHeight}px`, width: `${495.68679495687 / 746 * 
             currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * Number(currentViewportWidthIsLarger) / 2}px`}}>
@@ -161,30 +189,7 @@ const Contacts = ({data, currentViewportHeight, currentViewportWidthIsLarger, cu
                 `} />
                 Sorry, your browser does not support inline SVG.
             </svg> 
-            <div className='Contacts-Inner-Container' style={{height: `${550.76310550763 / 746 * currentViewportHeight}px`, 
-            maxWidth: `${512.2096881221 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
-            Number(currentViewportWidthIsLarger) / 2}px`}}>
-                <div className="Contacts-inner-content-container">
-                            <h4 className="Contacts-inner-container-heading">{data.contacts.address.heading}</h4>
-                        <div className="Contacts-inner-content-container Contacts-Items-Container">
-                            <div className="Contacts-text-container" style={{maxWidth: `${512.2096881221 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
-            Number(currentViewportWidthIsLarger) / 2}px`}}><p className="Contacts-item">
-                                    <img className="Contacts-icon" alt="Icon" src={require(`../img/contacts_location_icon.png`)}></img>
-                                    {data.contacts.address.text}</p></div>
-                            <div><p className="Contacts-item"><a className="Contacts-item" href={fullPhoneNumber}>
-                                    <img className="Contacts-icon" alt="Icon" src={require(`../img/contacts_call_icon.png`)}></img>
-                                    {data.header.phoneNumber}</a></p></div>
-                            <div><p className="Contacts-item"><a className="Contacts-item" href={fullEmail}>
-                                    <img className="Contacts-icon" alt="Icon" src={require(`../img/contacts_email_icon.png`)}></img>
-                                    {data.header.email}</a></p></div>
-                        </div>
-                </div>
-                <div className="Contacts-inner-content-container Contacts-Bottom-Text-Container">
-                            <h4 className="Contacts-inner-container-heading Contacts-second-inner-container-heading">{data.contacts.address.detailsHeading}</h4>
-                            <p style={{maxWidth: `${512.2096881221 / 746 * currentViewportHeight - 377.27272727273 / 746 * currentViewportHeight * 
-            Number(currentViewportWidthIsLarger) / 2}px`}} className="Contacts-inner-container-text">{data.contacts.address.details}</p>
-                </div>
-            </div>
+            
             <svg className='First-Page-Svg' style={{fill:"url(#SecondDecorElementGradient)", position: "absolute", width: "100%", top: "0", right: "0", height: "100%"}}>
                 <linearGradient id="SecondDecorElementGradient">
                 <stop offset="30%" stopColor="rgba(20,172,147,255)" />
