4d90eaded58f338fadd7d243bf87bda5d1334bfd
Ira1234567 <iraowl1234@gmail.com>
2023-09-17 20:37:46 +0300

    Update

--- ferrometals/src/header-and-footer/Callback.js
+++ ferrometals/src/header-and-footer/Callback.js
@@ -89,6 +89,7 @@ export default function Callback ({data, open, handleClose, handleToggle, classn
           <input style={{backgroundColor: "white"}} name="user_phone_number" type="text" id="user_phone_number" 
           className='Callback-textfield' onChange={event => {phoneNumberValidation(event.target.value, phoneNumberSymbols)}} 
           value={phoneNumberValue} placeholder={data.header.callback.phoneText}></input>
+          <input name="_formsubmit_id" type="text" style={{display: "none"}}></input>
           <button className='Callback-inner-button Callback-submit-button' value="Submit" type="submit">
             <span>{data.header.callback.buttonText}</span><ArrowForwardIosIcon className='Callback-inner-button-icon' /> 
           </button>
