d387cfa36a3f9c2cf4a41e794c8c43f992c2eefa
Ira1234567 <iraowl1234@gmail.com>
2023-09-30 21:55:59 +0300

    Update

--- ferrometals/src/nav/Nav.js
+++ ferrometals/src/nav/Nav.js
@@ -12,29 +12,29 @@ const Nav = ({data, changeHeaderBackground, navigateToTheTop, linkScroll}) => {
                 <Link  to={data.menuPaths.products} className='Nav-menu-item Dropdown-links-container' 
                 onClick={linkScroll}>{data.menuItems.products}<ExpandMore/></Link>
                 <div className="w3-dropdown-content w3-bar-block w3-card-4 Dropdown-Links-Inner-Container Dropdown-Container-With-Paddings">
-                    <Link className='Dropdown-link' to={data.menuPaths.transportingAndNearMachineEquipmentAndMachinesForWoodworking} 
+                    <Link className='Dropdown-link Dropdown-link-Margins First-Dropdown-link' to={data.menuPaths.transportingAndNearMachineEquipmentAndMachinesForWoodworking} 
                     onClick={linkScroll}>{data.menuItems.transportingAndNearMachineEquipmentAndMachinesForWoodworking}</Link> 
                     <br/>
-                    <Link className='Dropdown-link' to={data.menuPaths.equipmentForWasteProcessing} onClick={linkScroll}>
+                    <Link className='Dropdown-link Dropdown-link-Margins' to={data.menuPaths.equipmentForWasteProcessing} onClick={linkScroll}>
                         {data.menuItems.equipmentForWasteProcessing}</Link>
                     <br/>
-                    <Link className='Dropdown-link' to={data.menuPaths.roundWoodAccountingScanner} onClick={linkScroll}>
+                    <Link className='Dropdown-link Dropdown-link-Margins' to={data.menuPaths.roundWoodAccountingScanner} onClick={linkScroll}>
                         {data.menuItems.roundWoodAccountingScanner}</Link>
                     <br/>
-                    <Link className='Dropdown-link' to={data.menuPaths.polymerPaintingLines} onClick={linkScroll}>
+                    <Link className='Dropdown-link Dropdown-link-Margins' to={data.menuPaths.polymerPaintingLines} onClick={linkScroll}>
                         {data.menuItems.polymerPaintingLines}</Link>
                     <br/>
-                    <Link className='Dropdown-link' to={data.menuPaths.palletEquipment} onClick={linkScroll}>{data.menuItems.palletEquipment}</Link>
+                    <Link className='Dropdown-link Dropdown-link-Margins' to={data.menuPaths.palletEquipment} onClick={linkScroll}>{data.menuItems.palletEquipment}</Link>
                 </div>
             </div>
             <Link className='Nav-menu-item' to={data.menuPaths.catalog} onClick={linkScroll}>{data.menuItems.catalog}</Link>
-            <div className="w3-dropdown-hover ">
+            <div className="w3-dropdown-hover">
                 <Link className='Nav-menu-item Dropdown-links-container' to={data.menuPaths.gallery} onClick={linkScroll}>
                     {data.menuItems.gallery}<ExpandMore/></Link>
                 <div className="w3-dropdown-content w3-bar-block w3-card-4 Small-Dropdown-Links-Inner-Container Dropdown-Container-With-Paddings">
-                    <Link className='Dropdown-link' to={data.menuPaths.photo} onClick={linkScroll}>{data.menuItems.photo}</Link> 
+                    <Link className='Dropdown-link Dropdown-link-Margins' to={data.menuPaths.photo} onClick={linkScroll}>{data.menuItems.photo}</Link> 
                     <br/>
-                    <Link className='Dropdown-link' to={data.menuPaths.video} onClick={linkScroll}>{data.menuItems.video}</Link>
+                    <Link className='Dropdown-link Dropdown-link-Margins' to={data.menuPaths.video} onClick={linkScroll}>{data.menuItems.video}</Link>
                 </div>
             </div>
             <Link className='Nav-menu-item' to={data.menuPaths.contacts} onClick={navigateToTheTop}>{data.menuItems.contacts}</Link>
